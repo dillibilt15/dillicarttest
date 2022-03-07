f3fbad3a57199902da6d6ffacb5c3d2a195fb46e
Dillibabu <dillibilt15@gmail.com>
2022-03-07 20:29:47 +0530

    remove cart

--- app/Controllers/UserControllers/Plans.php
+++ app/Controllers/UserControllers/Plans.php
@@ -5,19 +5,37 @@ namespace App\Controllers\UserControllers;
 use App\Models\FileCapacityModel;
 use App\Models\StorageCapacityModel;
 use App\Models\UnlimitedPlansModel;
+use App\Models\UserCartModel;
 class Plans extends UserBaseController
 {
     
    
     public function index()
     {
-        $data['main_content']='UserViews/plans';
-        $data['header_title']='Plans';
-
- 
         $file_model=new FileCapacityModel();
         $storage_model=new StorageCapacityModel();
         $ul_model=new UnlimitedPlansModel();
+        $user_cart_model = new UserCartModel();
+        $data['main_content']='UserViews/plans';
+        $data['header_title']='Plans';
+        $data['cart_details']=array();
+       
+        $loged_in_user_id=$this->session->get('user_data')['id'];
+       
+        $user_cart_details=$this->get_cart_details($loged_in_user_id,$user_cart_model);
+        if (count( $user_cart_details)>0)
+        {
+            if ($user_cart_details[0]['cart_details']=='')
+            {
+               
+            }
+            else{
+                $data['cart_details']=json_decode($user_cart_details[0]['cart_details']);
+               
+            }
+        }
+ 
+       
 
         $data['files_pl_data']= $file_model->findAll();
         $data['storage_pl_data']= $storage_model->findAll();
@@ -25,7 +43,13 @@ class Plans extends UserBaseController
 
         return view('UserViews/user_main_page',$data);
     }
-
+    private function get_cart_details($loged_in_user_id,  $user_cart_model )
+    {
+        
+         $multiClause = array('user_id' =>$loged_in_user_id);
+         return $user_cart_model->where($multiClause)->orderBy('id','desc')->findAll();
+    }
+ 
    
 
    
--- app/Views/UserViews/plans.php
+++ app/Views/UserViews/plans.php
@@ -16,12 +16,12 @@
 <div class="tab-content" id="myTabContent">
   <div class="tab-pane fade show active" id="storage" role="tabpanel" aria-labelledby="storage-tab">
       Storage
-     <?php echo prepare_table_files_storage($storage_pl_data,'GB',$unlimited_pl_data) ?>
+     <?php echo prepare_table_files_storage($storage_pl_data,'GB',$unlimited_pl_data,$cart_details) ?>
 
 </div>
   <div class="tab-pane fade" id="files" role="tabpanel" aria-labelledby="files-tab">
       Files
-      <?php echo prepare_table_files_storage($files_pl_data,'Files',$unlimited_pl_data) ?>
+      <?php echo prepare_table_files_storage($files_pl_data,'Files',$unlimited_pl_data,$cart_details) ?>
     
 
   </div>
@@ -30,32 +30,32 @@
 </div>
 
 <?php 
-function prepare_table_files_storage($all_records,$type,$unlimited_pl_data)
+function prepare_table_files_storage($all_records,$type,$unlimited_pl_data,$cart_details)
 {
     $tr_html='<div class="row">';
     $sr_no=1;
     foreach($all_records as $rec)
     {
-        $tr_html= $tr_html.get_card_html($rec,'Monthly',$type,$rec['m_amount'], $sr_no);
+        $tr_html= $tr_html.get_card_html($rec,'Monthly',$type,$rec['m_amount'], $sr_no,$cart_details);
         $sr_no++;
-        $tr_html= $tr_html.get_card_html($rec,'Yearly',$type,$rec['y_amount'], $sr_no);
+        $tr_html= $tr_html.get_card_html($rec,'Yearly',$type,$rec['y_amount'], $sr_no,$cart_details);
         $sr_no++;    
     }
     foreach($unlimited_pl_data as $rec)
     {
         if (($type=='GB')&&($rec['plan_name']=='Storage'))
         {
-            $tr_html= $tr_html.get_card_html($rec,'Monthly',$type,$rec['m_amount'], $sr_no);
+            $tr_html= $tr_html.get_card_html($rec,'Monthly',$type,$rec['m_amount'], $sr_no,$cart_details);
             $sr_no++;
-            $tr_html= $tr_html.get_card_html($rec,'Yearly',$type,$rec['y_amount'], $sr_no);
+            $tr_html= $tr_html.get_card_html($rec,'Yearly',$type,$rec['y_amount'], $sr_no,$cart_details);
             $sr_no++;
         }
         
         if (($type=='Files')&&($rec['plan_name']=='Files'))
         {
-            $tr_html= $tr_html.get_card_html($rec,'Monthly',$type,$rec['m_amount'], $sr_no);
+            $tr_html= $tr_html.get_card_html($rec,'Monthly',$type,$rec['m_amount'], $sr_no,$cart_details);
             $sr_no++;
-            $tr_html= $tr_html.get_card_html($rec,'Yearly',$type,$rec['y_amount'], $sr_no);
+            $tr_html= $tr_html.get_card_html($rec,'Yearly',$type,$rec['y_amount'], $sr_no,$cart_details);
             $sr_no++;
         }
                    
@@ -64,7 +64,25 @@ function prepare_table_files_storage($all_records,$type,$unlimited_pl_data)
 
                 return   $tr_html.'</div>';
 }
-function get_card_html($rec,$validity,$type,$price,$sr_id)
+function cart_item_found($cart_data,$plan_id,$capacity,$validity,$f_type)
+{
+    $isfound=false;
+    foreach($cart_data as $d_rec)
+    {
+        if  (   ($d_rec->plan_id==$plan_id) &&
+                ($d_rec->capacity==$capacity) &&
+                ($d_rec->validity==$validity) &&
+                ($d_rec->f_type==$f_type) 
+            ) 
+        {
+            $isfound=true;
+            break;
+        }
+        
+    }
+    return $isfound;
+}
+function get_card_html($rec,$validity,$type,$price,$sr_id,$cart_details)
 {
     $capacity='Unlimited';
     if ($rec['capacity']>0)
@@ -72,7 +90,14 @@ function get_card_html($rec,$validity,$type,$price,$sr_id)
         $capacity=rtrim(rtrim(number_format($rec['capacity'],2),0),'.');
     }
   $card_id='c_'.$type."_".$sr_id;
-  return  '<div class="card col col-md-4 '.$type.'" id="'.$card_id.'" >
+  $item_is_found=cart_item_found($cart_details,$rec['id'],$capacity,$validity,$type);
+
+  $class_found=' ';
+  if ($item_is_found)
+  {
+    $class_found=' cart_exist ';
+  }
+  return  '<div class="card col col-md-4 '.$type.$class_found.' " id="'.$card_id.'" >
                             <div class="card-header">
                                 '. $capacity.' '. $type.' / '.$validity.'
                             </div>
@@ -226,5 +251,32 @@ function make_opacity_itmes(btnObj,card_id)
    $(btnObj).hide();
    $('#'+card_id).find('.remove_from_cart_btn').show();
    $('#'+card_id).find('.remove_from_cart_btn').prop('disabled', false);
+}
+
+$(document).ready(function(){
+   if ($('.GB.cart_exist').length>0)
+   {
+    load_time_itmes_opacity('GB');
+
+   }
+   if ($('.Files.cart_exist').length>0)
+   {
+    load_time_itmes_opacity('Files');
+
+   }
+    
+});
+
+function load_time_itmes_opacity(f_type)
+{
+    var ftype_cl='.'+f_type;
+    var f_foun_cls=ftype_cl+'.cart_exist';
+    $(ftype_cl).css('opacity','0.5');
+    $(f_foun_cls).css('opacity','');
+    $(ftype_cl).find('input').prop('disabled', true);
+
+     $(f_foun_cls).find('.add_to_cart_btn').hide();
+     $(f_foun_cls).find('.remove_from_cart_btn').show();
+     $(f_foun_cls).find('.remove_from_cart_btn').prop('disabled', false);
 }
     </script>
